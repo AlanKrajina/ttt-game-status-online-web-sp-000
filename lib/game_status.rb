73aa0998b122fad68403bf9557dea006ce79f29d
Alan Krajina <alankrajina88@gmail.com>
2019-05-03 19:53:41 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,32 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5], 
+  [6,7,8], 
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+
+def won?(board)
+  
+  WIN_COMBINATIONS.each do |comb|
+    comb.each do |win_comb|
+      
+      win_comb.map
+      
+      if position_taken?(board, index)
+      
+    end
+  end  
+    
+    
+    
+   else
+     false 
+  end
